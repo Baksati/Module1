d65f819c21fe1d82bcfabcecae50a0f2442185e4
Baksati <127687166+Baksati@users.noreply.github.com>
2023-03-20 21:41:26 +0300

    Initial commit

---
+++ src/CharDemo2 .java
@@ -0,0 +1,13 @@
+// Символьные переменные ведут себя
+//как целочисленные значения
+class CharDemo2 {
+    public static void main(String args[]) {
+        char ch1;
+
+        ch1 = 'x';
+        System.out.println(" ch1 содержит " + ch1);
+
+        ch1++; // увеличить на единицу значение переменной ch1
+        System.out.println(" ch1 теперь содержит " + ch1);
+    }
+}
